6901738dd2767690d748c2cf9546e9701a032699
Terry Nguyen <terryn@aie.edu>
2021-10-29 12:36:17 -0700

    Send collision events

---
+++ KinematicBody/Samples/SampleCollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleCollisionHandler : MonoBehaviour, IKinematicMotorCollisionHandler
+{
+    public void OnKinematicCollision(KinematicBody.MoveCollision collision)
+    {
+        Debug.Log("Collided with " + collision.otherCollider.name);
+    }
+
+    public void OnKinematicTrigger(KinematicBody.MoveCollision trigger)
+    {
+        Debug.Log("Triggered with " + trigger.otherCollider.name);
+    }
+}
