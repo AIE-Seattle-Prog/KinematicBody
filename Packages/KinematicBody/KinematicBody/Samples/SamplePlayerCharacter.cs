107433f4dac673a23b210cab029d05a7de85e9e8
Terry Nguyen <terryn@aie.edu>
2021-11-18 12:56:23 -0800

    Fix step height value

--- Packages/KinematicBody/KinematicBody/Samples/SamplePlayerCharacter.cs
+++ Packages/KinematicBody/KinematicBody/Samples/SamplePlayerCharacter.cs
@@ -11,10 +11,21 @@ public class SamplePlayerCharacter : MonoBehaviour
     // The motor we're controlling
     public KinematicPlayerMotor motor;
 
+    public bool useInputOverride = false;
+    public Vector3 inputOverride;
+
     private void Update()
     {
         // send inputs to motor
-        motor.MoveInput(new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")));
+        if (!useInputOverride)
+        {
+            motor.MoveInput(new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")));
+        }
+        else
+        {
+            motor.MoveInput(inputOverride);
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             motor.JumpInput();
